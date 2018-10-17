125e32af144f5af5a7881b01dc0a4200a30edfac
lixiaohuiobj <3454240494@qq.com>
2018-10-17 15:02:19 +0800

    hehe

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+@import url("../static/fonts/iconfont.css");
+*{
+  margin: 0;
+  padding:0;
+  text-decoration: none;
+  list-style: none;
+  box-sizing: border-box;
+}
+html,body,#app,.index{
+  width: 100%;
+  height: 100%;
+
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
+
---
+++ src/views/img.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="img">
+		<div class="img-top">
+			<p>颜色</p>
+			<p>车款</p>
+		</div>	
+	</div>
+</template>
+
+<script>
+	import { mapState, mapActions } from "vuex";
+	export default {
+		name: 'Detail',
+		data () {
+			return {
+
+			}
+		},
+		computed:{
+			...mapState({
+				imgList: (state) => state.img.imgList
+			})
+		},
+		methods:{
+			...mapActions({
+				getImgList:"img/getImgList"
+			})
+		},
+		mounted(){
+			this.getImgList(this.$route.query.serialID)
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.img{
+		width: 100%;
+		height: .8rem;
+	}
+	.img-top{
+		width: 100%;
+		display: flex;
+	}
+	.img-top p{
+		text-align: center;
+		flex: 1;
+	    display: inline-block;
+	    vertical-align: middle;
+	    font-size: .28rem;
+		line-height: .8rem;
+	}
+	.img-top p:after{
+		content: "";
+	    display: inline-block;
+	    padding-top: .16rem;
+	    padding-right: .16rem;
+	    border-top: 2px solid #ccc;
+	    border-right: 2px solid #ccc;
+	    -webkit-transform: rotate(135deg);
+	    transform: rotate(135deg);
+	    margin-left: .2rem;
+	    vertical-align: 5%;
+	}
+</style>
